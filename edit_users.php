d7e823f157a37e7dea8294f918434b13cb840347
Diego <diegonrriqueaz123@hotmail.com>
2023-04-19 19:46:38 -0300

    Adicionado comentários explicativos do códigoAdicionado comentários explicativos do código

--- edit_users.php
+++ edit_users.php
@@ -9,6 +9,9 @@
 </head>
 <body>
     <?php
+    // começo validando o ID como um tipo inteiro e logo em seguida 
+    // agrego código SQL a variavel para realizar uma injeção no Banco de Dados
+    // e o Fetch para fazer um loop nesse Banco
     $id = intval($_GET['id']);
     $sql_modify = "SELECT * FROM funcionarios WHERE id = '$id'";
     $query_modify = $mysqli->query($sql_modify);
@@ -16,6 +19,8 @@
     
     ?>
 <form action="" method = "POST">
+    <!-- Utilizo o Post para captar as informações das input.
+    O uso echo no value para dar uma sensação de alteração em tempo real dos dados-->
     <fieldset>
         <legend>Alterar Usuario</legend>
         <input type="text" name="nome"  value="<?php echo $cliente['Nome']?>">
@@ -23,11 +28,16 @@
         
         <button type="submit">Alterar</button>
     <?php 
+    // Verifico se existe alguma variavel no método POST.
+    // Caso exista agrego valor a eles utilizando a memoria do POST
     if(count($_POST)>0) {
         $nome = $_POST['nome'];
         $email = $_POST['email'];
         $error = array();
-        
+        //Aqui faço validações de INPUTS em brancos, Email digitado corretamente e por fim
+        // uma busca na variavel error para seguir com o código 
+        // Caso a variavel esteja vazia significa que o código rodou de forma correta 
+        // sem gerar erros na memória 
             if(!empty($nome) and !empty($email)){
                 if(filter_var($email, FILTER_VALIDATE_EMAIL)){
                     if(empty($error) == 1){
@@ -37,14 +47,15 @@
                         $query_change =$mysqli->query($sql);
                         echo "Usuario alterado com sucesso!!!";
                     }
-
+                    // Feedback de erros
                 } else {
                     echo "Preencha o email corretamente.";
+                    
                 }
 
             } else {
                 echo "    Preencha todos os campos.";
-                $error = 1;
+                
                 
             }
     }
